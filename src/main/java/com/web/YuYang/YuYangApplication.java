57b1a919ab78641f3696c7ee06223540b4ea7ab5
liuyuhg <hunter@hunterdembp.cn.ibm.com>
2020-06-11 21:48:13 +0800

    Init

---
+++ src/main/java/com/web/YuYang/YuYangApplication.java
@@ -0,0 +1,13 @@
+package com.web.YuYang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuYangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuYangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/YuYang/YuYangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.YuYang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuYangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
